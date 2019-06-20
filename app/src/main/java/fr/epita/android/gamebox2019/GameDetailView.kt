3fbe1d2eb5fefdb37d90469c9c0d2336424c0940
Florian Padel <florian.padel@epita.fr>
2019-06-20 17:21:44 +0200

    [gamedetailview] started to implem game detail fragment

---
+++ app/src/main/java/fr/epita/android/gamebox2019/GameDetailView.kt
@@ -0,0 +1,80 @@
+package fr.epita.android.gamebox2019
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.google.gson.GsonBuilder
+import kotlinx.android.synthetic.main.fragment_game_detail_view.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+class GameDetailsView : Fragment() {
+    interface OnFragmentInteractionListener {
+        fun onFragmentInteraction(){}
+    }
+
+    var game_id : Int = 0;
+
+    fun setGameId(game : Int) {
+        this.game_id = game
+    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val rootView = inflater.inflate(R.layout.fragment_game_detail_view, container, false)
+
+        return rootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val baseURL = "https://androidlessonsapi.herokuapp.com/api/"
+        val jsonConverter = GsonConverterFactory.create(GsonBuilder().create())
+        val retrofit = Retrofit.Builder()
+            .baseUrl(baseURL)
+            .addConverterFactory(jsonConverter)
+            .build()
+
+        val service: WebServiceInterface = retrofit.create(WebServiceInterface::class.java)
+
+        val call = game_id?.let { service.getGameDetail(it) }
+
+        call?.enqueue(object : Callback<GameDetail> {
+            override fun onFailure(call: Call<GameDetail>, t: Throwable) {
+                Log.w("MainActivity", "WS Error")
+            }
+
+            override fun onResponse(call: Call<GameDetail>, response: Response<GameDetail>) {
+                if (response.code() == 200) {
+                    val data = response.body()
+                    Toast.makeText(activity, "popup", Toast.LENGTH_SHORT).show()
+                    /*game_name.text = data?.name
+                    game_type.text = data?.type
+                    nb_player.text = "Nombre de joueurs: " + data?.players.toString()
+                    activity?.let { Glide.with(it).load(data?.picture).into(game_picture) }
+                    description_fr.text = data?.description_fr
+                    description_en.text = data?.description_en
+                    more_button.setOnClickListener {
+                        val uri: Uri = Uri.parse(data?.url);
+                        val intent = Intent(Intent.ACTION_VIEW, uri);
+                        startActivity(intent);
+                    }*/
+                }
+            }
+        })
+    }
+
+
+    }
--- app/src/main/java/fr/epita/android/gamebox2019/MainActivity.kt
+++ app/src/main/java/fr/epita/android/gamebox2019/MainActivity.kt
@@ -3,6 +3,7 @@ package fr.epita.android.gamebox2019
 import android.content.pm.ActivityInfo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,4 +18,24 @@ class MainActivity : AppCompatActivity() {
         fragmentTransaction.replace(R.id.main_container, GameList)
         fragmentTransaction.commit()
     }
+
+    fun showGameDetails(view : View) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        val gameDetails = GameDetailsView()
+        gameDetails.setGameId(view.id)
+
+        fragmentTransaction.replace(R.id.main_container, gameDetails)
+        fragmentTransaction.commit()
+    }
+
+    fun previous_main_page(view : View) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        val GameList = GameList()
+        fragmentTransaction.replace(R.id.main_container, GameList)
+
+        fragmentTransaction.commit()
+    }
+
+
+
 }
